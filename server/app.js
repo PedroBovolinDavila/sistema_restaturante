d4dbe63f5d22f49fc577ccd629dd9ac94f474496
Pedro Davila <pedrodavila.bovolin@gmail.com>
2022-02-19 21:41:01 -0300

    Database Configured

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 
+// Imports
+
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const path = require('path');
+
+// App init and Config
 
 const app = express();
 
@@ -14,6 +18,14 @@ app.use(routes);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '..', '/public/views'));
+
+// MongoDB Connection
+
+mongoose.connect(process.env.DATABASE_URL)
+  .then(() => console.log('Connected to Database'))
+  .catch(err => console.log(err));
+
+// Open server
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
