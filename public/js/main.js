56d410154003fa5108096a84802deb0101dc8686
Pedro Davila <pedrodavila.bovolin@gmail.com>
2022-02-20 13:02:43 -0300

    Login refatorado e navbar adicionada

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,31 @@
-let userId = document.cookie.split('=')[1];
+const userId = document.cookie.split('=')[1];
+const buttonMenu = document.querySelector('.toggle-menu');
+const dropdown = document.querySelector('.dropdown');
 
-fetch(`/users/validate/${userId}`, { method: 'post' })
-  .then(res => res.json())
-  .then(data => {
-    if (!data.logged) {
-      window.location.href = '/login';
-    }
-  });
+function validateLogin(id) {
+  if (!id.trim()) {
+    window.location.href = '/login';
+  }
+
+  fetch(`/users/validate/${id}`, { method: 'post' })
+    .then(res => res.json())
+    .then(data => {
+      if (!data.logged) {
+        window.location.href = '/login';
+      }
+    });
+
+  localStorage.setItem('userId', userId);
+}
+validateLogin(userId);
+
+function deslogar() {
+  document.cookie = 'userId='
+  localStorage.removeItem('userId');
+  let userId = document.cookie.split('=')[1];
+  validateLogin(userId)
+}
+
+function showMenu() {
+  dropdown.classList.toggle('hide');
+}
